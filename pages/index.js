bd54a42613738ebfeb06d3dc33b5bee922a18721
bmlee531@gmail.com <bmlee531@gmail.com>
2022-06-25 11:12:47 +0900

    feat: next theme
    - next-themes 라이브러리로 theme 세팅

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
 import Head from 'next/head';
+import { useTheme } from 'next-themes';
 import { DATABASE_ID } from '../lib/notionKey';
 import { getDatabase } from '../lib/notion';
+import { useEffect, useState } from 'react';
 
 export default function Home({ posts }) {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
   return (
     <div>
       <h1>nextjs notion blog</h1>
+      <div>
+        The current theme is: {theme}
+        <button onClick={() => (theme === 'dark' ? setTheme('light') : setTheme('dark'))}>theme toggle</button>
+      </div>
     </div>
   );
 }
