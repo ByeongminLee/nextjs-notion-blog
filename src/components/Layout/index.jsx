7f0c3fd4e360c053ba59810c2833c75bccd3f5e6
bmlee531@gmail.com <bmlee531@gmail.com>
2022-07-03 11:54:24 +0900

    feat: layout components

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,3 +1,4 @@
+import styled from '@emotion/styled';
 import Navbar from './navbar';
 import Footer from './footer';
 
@@ -5,10 +6,25 @@ const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
-      <main>{children}</main>
+      <Container>
+        <Wrapper>
+          <main>{children}</main>
+        </Wrapper>
+      </Container>
       <Footer />
     </>
   );
 };
 
 export default Layout;
+
+const Container = styled.div`
+  width: 100%;
+  margin: 70px 0;
+`;
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 940px;
+  padding: 0 20px;
+`;
