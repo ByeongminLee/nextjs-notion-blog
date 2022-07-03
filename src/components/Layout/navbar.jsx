780051a568b56c35f0a417436c11016feaa0f5c8
bmlee531@gmail.com <bmlee531@gmail.com>
2022-07-03 11:53:44 +0900

    feat: navbar components

--- src/components/Layout/navbar.jsx
+++ src/components/Layout/navbar.jsx
@@ -1,5 +1,40 @@
+import styled from '@emotion/styled';
+
 const navbar = () => {
-  return <div>navbar</div>;
+  return (
+    <Container>
+      <Wrapper>
+        <div>
+          <h1>nextjs-notion-blog</h1>
+        </div>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default navbar;
+
+const Container = styled.div`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  background-color: var(--color-navbar-bg);
+  border-bottom: 1px solid var(--color-navbar-border);
+`;
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 940px;
+  padding: 0 20px;
+  height: 60px;
+  line-height: 20px;
+
+  div {
+    display: flex;
+    justify-content: space-between;
+    h1 {
+      font-size: var(--fontSize-xl);
+      font-weight: 600;
+    }
+  }
+`;
