55a2abd40a67f950c34fe0bb3bd97dba3dfc8e61
bmlee531@gmail.com <bmlee531@gmail.com>
2022-07-07 21:53:41 +0900

    update: add button componets
    - button in postsContainer

--- src/components/PostsContainer/index.jsx
+++ src/components/PostsContainer/index.jsx
@@ -1,7 +1,9 @@
 import styled from '@emotion/styled';
+import Button from '../Button';
 import PostCard from './PostCard';
 
 const PostsContainer = ({ dataList }) => {
+  const moreHandler = () => {};
   return (
     <div>
       {dataList?.map((data, key) => (
@@ -9,6 +11,9 @@ const PostsContainer = ({ dataList }) => {
           <PostCard data={data} />
         </Container>
       ))}
+      <MoreContainer>
+        <Button text={'MORE'} onClick={moreHandler} />
+      </MoreContainer>
     </div>
   );
 };
@@ -17,4 +22,10 @@ export default PostsContainer;
 
 const Container = styled.div`
   padding: 0 10px 70px 10px;
+`;
+
+const MoreContainer = styled.div`
+  width: 100px;
+
+  margin: 0 auto;
 `;
