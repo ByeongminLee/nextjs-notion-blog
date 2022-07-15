c9677740434c8b26ae9a8f9ddbb291b2bd8e4c12
bmlee531@gmail.com <bmlee531@gmail.com>
2022-07-16 00:34:09 +0900

    feat: Meta Components for SEO

---
+++ src/components/Meta/index.jsx
@@ -0,0 +1,49 @@
+import Head from 'next/head';
+
+const Meta = props => {
+  const { title, description, url, image, favicon } = props;
+
+  return (
+    <Head>
+      {/* 기본 */}
+      <title>{title}</title>
+      <html lang="ko" />
+      <meta name="description" content={description} />
+      <meta name="keywords" content="Site Keywords1 ,Site Keywords2, Site Keywords3" />
+      <meta name="language" content="Korean" />
+      <meta name="author" content="byeongminLee" />
+      <meta name="robots" content="index, follow" />
+      <link rel="icon" type="image/png" href={favicon} />
+      {/* Facebook */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={url} />
+      <meta property="og:title" content={title} />
+      <meta property="og:image" content={image} />
+      <meta property="og:description" content={description} />
+      <meta property="og:site_name" content={title} />
+      <meta property="og:locale" content="ko" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      {/* Apple */}
+      <link rel="apple-touch-icon" href="/custom-icon.png" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:url" content={url} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
+    </Head>
+  );
+};
+
+export default Meta;
+
+Meta.defaultProps = {
+  title: 'nextjs-notion-blog',
+  description: 'nextjs blog using notion API',
+  url: 'http://localhost:3000/',
+  image: 'http://localhost:3000/image.png',
+  favicon: 'http://localhost:3000/favicon.png',
+};
